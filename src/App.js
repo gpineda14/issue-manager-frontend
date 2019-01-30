0b390ad498d075901ec5d3a0079d5540a5f1b241
Guillermo Pineda <pinedag395@gmail.com>
2019-01-30 15:51:17 -0800

    Implemented two canvas elements

--- src/App.js
+++ src/App.js
@@ -4,19 +4,28 @@ import './App.css';
 class ImageCanvas extends Component {
   constructor(props) {
     super(props);
-    this.handleMouseDown = this.handleMouseDown.bind(this);
   }
 
-  handleMouseDown(event) {
-      const x = event.x;
-      const y = event.y;
-      console.log(x, y);
+  componentDidMount() {
+    const image_layer = this.refs.image_layer;
+    const drawing_layer = this.refs.drawing_layer;
+
+    const image_ctx = image_layer.getContext("2d");
+    const drawing_ctx = drawing_layer.getContext("2d");
+
+    const img = new Image();
+    img.src = "https://static.techspot.com/images2/news/bigimage/2018/09/2018-09-04-image-6.png";
+    img.onload = () => {
+      image_ctx.drawImage(img, 0, 0, 500, 500);
+    }
   }
 
   render() {
     return(
       <div className="canvas">
-        <canvas id="canvas" onMouseDown={(e) => this.handleMouseDown(e.nativeEvent)}>
+        <canvas ref="image_layer" className="image_layer">
+        </canvas>
+        <canvas ref="drawing_layer" className="drawing_layer">
         </canvas>
       </div>
     )
