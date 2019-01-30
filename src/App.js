3dd851228b0ab244e71f39a7499ab1aafd0df308
Guillermo Pineda <pinedag395@gmail.com>
2019-01-29 19:04:24 -0800

    Added Image Canvas

--- src/App.js
+++ src/App.js
@@ -1,27 +1,33 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+class ImageCanvas extends Component {
+  constructor(props) {
+    super(props);
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(event) {
+      const x = event.x;
+      const y = event.y;
+      console.log(x, y);
+  }
+
+  render() {
+    return(
+      <div className="canvas">
+        <canvas id="canvas" onClick={(e) => this.handleClick(e.nativeEvent)}>
+        </canvas>
+      </div>
+    )
+  }
+}
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+      <ImageCanvas />
+    )
   }
 }
 
